d7e282c1949559830b689466d0edd2f1dd4aefa5
Satish Ad47 <48507510+satishad4798@users.noreply.github.com>
2020-07-08 13:51:03 +0530

    Revert "Update config.php"
    This reverts commit 800bf6a4f88783b475147c1b388a8a0dabcaee5b.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
-$dbname = "video_organise_xx"; /* Database name */
+$dbname = "video_organise"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
